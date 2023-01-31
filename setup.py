4b9c2889dd9cbd0b4f47635a5c362bfc929953d9
Alexander James <akeenjames@gmail.com>
2023-01-31 12:48:22 -0800

    fix dependencies

--- setup.py
+++ setup.py
@@ -30,7 +30,7 @@ setup(
         "scipy>=1.7.1",
         "numpy>=1.21.5",
         "PyRQA>=8.0.0",
-        "sklearn>=1.2.1"
+        "scikit-learn>=1.2.1"
     ],
     python_requires=">=3.8.0"
 )
